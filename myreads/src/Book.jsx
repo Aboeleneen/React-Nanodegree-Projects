96b5199295271965751a506bb1e2e2313e6d0417
Aboeleneen <aboeleneen.98@yahoo.com>
2020-07-19 19:01:27 +0200

    refactor : update code to match style guides

--- myreads/src/Book.jsx
+++ myreads/src/Book.jsx
@@ -4,26 +4,30 @@ import React , {Component} from 'react' ;
 class Book extends Component {
 
   render(){
+    let cover = "https://ravenspacepublishing.org/wp-content/uploads/2019/04/default-book.jpg"
+    cover = this.props.book.imageLinks !== undefined ?  this.props.book.imageLinks.thumbnail : cover ;
     return(
       <div className="book">
          <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')' }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' +cover + ')' }}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="move" disabled>Move to...</option>
               <option onClick={(event)=>{this.props.updateBook(this.props.book , event.target.value)}}
                       value="currentlyReading">
-                      {this.props.book.shelf==="currentlyReading" && this.props.onShelf ? "✓ Currently Reading" :  "Currently Reading"}
+                      {this.props.book.shelf==="currentlyReading" && this.props.onShelf ? "✓ Currently Reading" :  "  Currently Reading"}
               </option>
               <option onClick={(event)=>{this.props.updateBook(this.props.book , event.target.value)}}
                       value="wantToRead">
-                      {this.props.book.shelf==="wantToRead"  && this.props.onShelf ? "✓ Want to Read" : "Want to Read"}
+                      {this.props.book.shelf==="wantToRead"  && this.props.onShelf ? "✓ Want to Read" : "  Want to Read"}
               </option>
               <option onClick={(event)=>{this.props.updateBook(this.props.book , event.target.value)}}
                       value="read">
-                      {this.props.book.shelf==="read"  && this.props.onShelf ? "✓ Read" : "Read"}
+                      {this.props.book.shelf==="read"  && this.props.onShelf ? "✓ Read" : "  Read"}
               </option>
-              <option value="none">None</option>
+              <option value="none">
+                      {this.props.onShelf ? "  None" : "✓ None"}
+              </option>
             </select>
           </div>
         </div>
