73c827cd2169a64a907ca2d745d4726c278ec12f
Aboeleneen <aboeleneen.98@yahoo.com>
2020-08-30 16:24:12 +0200

    feat: would-you-rather inital commit

---
+++ would-you-rather/src/components/App.js
@@ -0,0 +1,12 @@
+import React , {Component} from 'react'
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        StarterCode
+      </div>
+    )
+  }
+}
+export default App
